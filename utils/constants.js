190627710cc54fa183dff051d5a513583941533e
Sufyanakram16 <sufyanakram1223@gmail.com>
2023-01-15 15:20:36 +0500

    .json

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-import abi from '../build/contracts/Lottery.json'
+import abi from '../contracts/Lottery.json'
 export const contractAddress = '0x68d0B9439F2f266E10E27335dD3cdc23a70Ee886'
 
 export const contractABI = abi.abi;
